749c27e883c30ac8203f35feb7e2d1bd8655ee8b
KimYong <68097961+DaeeYong@users.noreply.github.com>
2023-04-14 15:33:58 +0900

    경로 유효성 확인 수도 코드

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,61 +1,79 @@
-#include<iostream>
-#include<utility>
-#include<queue>
-
-using namespace std;
-
-#define X first
-#define Y second
-#define BORD_SIZE 17
-
-#define VISITED 1
-#define PLAYER1 2
-#define PLAYER2 3
-#define BLOCK 4
-
-int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,1,0,-1 };
-
-int board[17][17];
-int visited[17][17];
-
-//벽->1, p1->2, p2 -> 3
-
-int isValid(pair<int, int> Coord, int playerType);
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    return 0;
-}
-
-int isValid(pair<int, int> Coord, int playerType) {
+bool isValidObstacle(vector<pair<int,int>> obstacleCoord, vector<pair<int,int>> playerCoord) {
     
+    _initialize();
+
+    pair<int, int> obstacle_F;
+    pair<int, int> obstacle_S;
+    
+    pair<int, int> player1 = playerCoord[0];
+    pair<int, int> player2 = playerCoord[1];
+
+    visited[obstacle_F.X][obstacle_F.Y] = BLOCKED;
+    visited[obstacle_S.X][obstacle_S.Y] = BLOCKED;
+
+    //For player1
+    bool flag1 = false;
+    queue<pair<int, int>> Q1;
+    Q1.push({ player1.X, player1.Y });
+    
+    while (!Q1.empty()) {
+        auto cur = Q1.front(); Q1.pop();
+        for (int direction = 0; direction < 4; direction++) {
+            int nx = dx[direction] + cur.X;
+            int ny = dy[direction] + cur.Y;
+
+            if (nx < 0 || nx >= BORD_SIZE || ny < 0 || ny >= BORD_SIZE) continue;
+            if (board[nx][ny] == BLOCKED || visited[nx][ny] == VISITED) continue;
+            if (ny == (BORD_SIZE - 1)) {
+                flag1 = true;
+                break;
+            }
+            //길 찾았을 경우
+
+            visited[nx][ny] = VISITED;
+            Q1.push({ nx,ny });
+        }
+        if (flag1 == true) break;
+    }
+
+    //For player2
+    bool flag2 = false;
+    queue<pair<int, int>> Q2;
+    Q2.push({ player1.X, player1.Y });
+
+    while (!Q2.empty()) {
+        auto cur = Q2.front(); Q2.pop();
+        for (int direction = 0; direction < 4; direction++) {
+            int nx = dx[direction] + cur.X;
+            int ny = dy[direction] + cur.Y;
+
+            if (nx < 0 || nx >= BORD_SIZE || ny < 0 || ny >= BORD_SIZE) continue;
+            if (board[nx][ny] == BLOCKED || visited[nx][ny] == VISITED) continue;
+            if (ny == (BORD_SIZE - 1)) {
+                flag1 = true;
+                break;
+            }
+            visited[nx][ny] = VISITED;
+            Q2.push({ nx,ny });
+        }
+        if (flag1 == true) break;
+    }
+
+    if (flag1 == true && flag2 == true) return true;
+    else return false;
+}
+void _initialize() {
+    //init visited
     for (int i = 0; i < BORD_SIZE; i++) {
         for (int j = 0; j < BORD_SIZE; j++) {
             visited[i][j] = 0;
         }
     }
 
+    //copy visited -> board
     for (int i = 0; i < BORD_SIZE; i++) {
         for (int j = 0; j < BORD_SIZE; j++) {
             visited[i][j] = board[i][j];
-        }
-    }
-    queue <pair<int, int>> Q;
-    
-    Q.push(Coord);
-    while (!Q.empty())
-    {
-        auto cur = Q.front(); Q.pop();
-        for (int direction = 0; direction < 4; direction++) {
-            int nx = dx[direction] + cur.X;
-            int ny = dy[direction] + cur.Y;
-            
-            if (nx < 0 || nx >= BORD_SIZE || ny < 0 || ny >= BORD_SIZE) continue;
-            if (board[nx][ny] == 1 || visited[nx][ny] == VISITED) continue;
-            visited[nx][ny] = VISITED;
-            Q.push({ nx,ny });
         }
     }
 }
