e1873b3487f9ee2e720f7e4b82b35dbe17fc7e9a
goguma1000 <102130574+goguma1000@users.noreply.github.com>
2023-05-11 12:34:38 +0900

    Update User.java

--- server/MSEproject/src/main/java/anido/MSEproject/domain/User.java
+++ server/MSEproject/src/main/java/anido/MSEproject/domain/User.java
@@ -1,7 +1,21 @@
 package anido.MSEproject.domain;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
+@Entity
+@Table(name = "users")
 public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+    
+    @Column(name = "name", nullable = false)
     private String name;
 
     public Long getId() {
@@ -11,12 +25,15 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
+   
 
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
+    
+    public User(){ }
 }
