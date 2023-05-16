644c8db12e050c97216b527b10b67b7cb955b210
KimYong <so155s@ajou.ac.kr>
2023-05-16 15:48:54 +0900

    .

---
+++ server/MSEproject/src/main/java/anido/MSEproject/controller/GameController.java
@@ -0,0 +1,7 @@
+package anido.MSEproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GameController {
+}
---
+++ server/MSEproject/src/main/java/anido/MSEproject/domain/Board.java
@@ -0,0 +1,45 @@
+package anido.MSEproject.domain;
+
+public class Board {
+
+    final int BOARD_SIZE = 17;
+    private int[][] board = new int[BOARD_SIZE][BOARD_SIZE];
+
+    public Board(){
+        for(int i=0; i<BOARD_SIZE; i++){
+            for(int j=0; j<BOARD_SIZE; j++){
+                this.board[i][j] = 0;
+            }
+        }
+    }
+
+    public int getBOARD_SIZE() {
+        return BOARD_SIZE;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void clear(){
+        for(int i=0; i<BOARD_SIZE; i++){
+            for(int j=0; j<BOARD_SIZE; j++){
+                setBoardValue(i,j,0);
+            }
+        }
+    }
+    public void setBoardValue(int y, int x, int value){
+        board[y][x] = value;
+    }
+    public int getBoardValue(int y, int x){
+        return board[y][x];
+    }
+
+    public void copyTo(Board dest){
+        for(int i=0; i<BOARD_SIZE; i++){
+            for(int j=0; j<BOARD_SIZE; j++){
+                dest.setBoardValue(i,j, board[i][j]);
+            }
+        }
+    }
+}
